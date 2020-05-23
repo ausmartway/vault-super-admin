f5d2a70d6ab8823bb4f09886394b850dd33c388d
Yulei Liu <yulei.liu@gmail.com>
2020-05-23 21:35:05 +1000

    remove dash

--- namespaces.tf
+++ namespaces.tf
@@ -1,8 +1,8 @@
-resource "vault_namespace" "ns-project1" {
-  path = "ns-project1"
+resource "vault_namespace" "nsproject1" {
+  path = "nsproject1"
 }
 
-resource "vault_namespace" "ns-project2" {
-  path = "ns-project2"
+resource "vault_namespace" "nsproject2" {
+  path = "nsproject2"
 }
 
