157e15f849a9f64f76e97251a42523272037b0c1
Yulei Liu <yulei.liu@gmail.com>
2020-05-25 14:18:47 +1000

    remove some comment

--- otherresources.tf
+++ otherresources.tf
@@ -2,10 +2,6 @@ resource "vault_namespace" "project1-namespace" {
   path = "project1"
 }
 
-// resource "vault_namespace" "nsproject2" {
-//   path = "nsproject2"
-// }
-
 resource "github_repository" "cba-vault-namespace-nsproject1" {
   name        = "cba-vault-namespace-nsproject1"
   description = "My awesome codebase"
