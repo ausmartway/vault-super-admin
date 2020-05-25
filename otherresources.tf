762b2bf4c9f8ebb5a22751bdfb070be65f92ae7a
Yulei Liu <yulei.liu@gmail.com>
2020-05-25 17:32:06 +1000

    try 3

--- otherresources.tf
+++ otherresources.tf
@@ -35,38 +35,38 @@ resource "vault_policy" "project1-namespace-admin-policy" {
   name="project1-namespace-admin-policy"
   policy=<<EOP
 # Manage namespaces
-path "sys/namespaces/*" {
+path "${vault_namespace.project1-namespace.path}/sys/namespaces/*" {
    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
 }
 
 # Manage policies
-path "sys/policies/acl/*" {
+path "${vault_namespace.project1-namespace.path}/sys/policies/acl/*" {
    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
 }
 
 # List policies
-path "sys/policies/acl" {
+path "${vault_namespace.project1-namespace.path}/sys/policies/acl" {
    capabilities = ["list"]
 }
 
 # Enable and manage secrets engines
-path "sys/mounts/*" {
+path "${vault_namespace.project1-namespace.path}/sys/mounts/*" {
    capabilities = ["create", "read", "update", "delete", "list"]
 }
 
 # List available secrets engines
-path "sys/mounts" {
+path "${vault_namespace.project1-namespace.path}/sys/mounts" {
   capabilities = [ "read" ]
 }
 
 # Create and manage entities and groups
-path "identity/*" {
+path "${vault_namespace.project1-namespace.path}/identity/*" {
    capabilities = ["create", "read", "update", "delete", "list"]
 }
 
 
 # Manage tokens
-path "auth/token/*" {
+path "${vault_namespace.project1-namespace.path}/auth/token/*" {
    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
 }
 EOP
