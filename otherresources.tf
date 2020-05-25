b3f79fb9546fcd82a1e5b06678e8fdb3eb274c5c
Yulei Liu <yulei.liu@gmail.com>
2020-05-25 12:30:58 +1000

    repo debug

--- otherresources.tf
+++ otherresources.tf
@@ -6,17 +6,17 @@ resource "vault_namespace" "nsproject1" {
 //   path = "nsproject2"
 // }
 
-// resource "github_repository" "cba-vault-namespace-nsproject1" {
-//   name        = "cba-vault-namespace-nsproject1"
-//   description = "My awesome codebase"
+resource "github_repository" "cba-vault-namespace-nsproject1" {
+  name        = "cba-vault-namespace-nsproject1"
+  description = "My awesome codebase"
 
-//   private = true
+  private = true
 
-//   template {
-//     owner = "ausmartway"
-//     repository = "vault-namespace-template"
-//   }
-// }
+  template {
+    owner = "ausmartway"
+    repository = "vault-namespace-template"
+  }
+}
 
 // resource "tfe_workspace" "project1-workspace" {
 //   name         = "project1-workspace"
