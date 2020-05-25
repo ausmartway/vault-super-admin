d53d5ea3feb636daebc4e049ddb1140d6e5939c1
Yulei Liu <yulei.liu@gmail.com>
2020-05-25 12:09:50 +1000

    typo

--- otherresources.tf
+++ otherresources.tf
@@ -22,7 +22,7 @@ resource "tfe_workspace" "project1-workspace" {
   name         = "project1-workspace"
   organization = "yulei"
   vcs_repo  {
-    identifier = "ausmartway/${github_repository.cba-vault-namespace-nsproject1.name}"
+    identifier = "cba-demo/${github_repository.cba-vault-namespace-nsproject1.name}"
     oauth_token_id = "ot-tkpMwbbMB59jSYiE"
   }
 }
