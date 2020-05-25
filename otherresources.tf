05a9f5dd8f40b560267615f11545dfa81fa72e0b
Yulei Liu <yulei.liu@gmail.com>
2020-05-25 12:16:25 +1000

    remove namesapce2

--- otherresources.tf
+++ otherresources.tf
@@ -2,9 +2,9 @@ resource "vault_namespace" "nsproject1" {
   path = "nsproject1"
 }
 
-resource "vault_namespace" "nsproject2" {
-  path = "nsproject2"
-}
+// resource "vault_namespace" "nsproject2" {
+//   path = "nsproject2"
+// }
 
 resource "github_repository" "cba-vault-namespace-nsproject1" {
   name        = "cba-vault-namespace-nsproject1"
