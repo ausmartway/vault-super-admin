762a19342bc4a2575efb5c2c49a865c38bf51221
Yulei Liu <yulei.liu@gmail.com>
2020-05-26 21:38:07 +1000

    temp remove of vault resources

--- otherresources.tf
+++ otherresources.tf
@@ -1,6 +1,3 @@
-resource "vault_namespace" "project1-namespace" {
-  path = "project1"
-}
 
 resource "github_repository" "cba-vault-namespace-nsproject1" {
   name        = "cba-vault-namespace-nsproject1"
@@ -31,67 +28,72 @@ resource "tfe_variable" "project1-workspace-namespace-vault-namespace" {
   value=vault_namespace.project1-namespace.path
 }
 
-resource "vault_policy" "project1-namespace-admin-policy" {
-  name="project1-namespace-admin-policy"
-  policy=<<EOP
-# Manage namespaces
-path "sys/namespaces/*" {
-   capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-}
+// resource "vault_namespace" "project1-namespace" {
+//   path = "project1"
+// }
 
-# Manage policies
-path "sys/policies/acl/*" {
-   capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-}
 
-# List policies
-path "sys/policies/acl" {
-   capabilities = ["list"]
-}
+// resource "vault_policy" "project1-namespace-admin-policy" {
+//   name="project1-namespace-admin-policy"
+//   policy=<<EOP
+// # Manage namespaces
+// path "sys/namespaces/*" {
+//    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+// }
 
-# Enable and manage secrets engines
-path "sys/mounts/*" {
-   capabilities = ["create", "read", "update", "delete", "list"]
-}
+// # Manage policies
+// path "sys/policies/acl/*" {
+//    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+// }
 
-# List available secrets engines
-path "sys/mounts" {
-  capabilities = [ "read" ]
-}
+// # List policies
+// path "sys/policies/acl" {
+//    capabilities = ["list"]
+// }
 
-# Create and manage entities and groups
-path "identity/*" {
-   capabilities = ["create", "read", "update", "delete", "list"]
-}
+// # Enable and manage secrets engines
+// path "sys/mounts/*" {
+//    capabilities = ["create", "read", "update", "delete", "list"]
+// }
 
-# Manage tokens
-path "auth/token/*" {
-   capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-}
+// # List available secrets engines
+// path "sys/mounts" {
+//   capabilities = [ "read" ]
+// }
 
-# Manage secrets at 'kv'
-path "kv/*" {
-   capabilities = ["create", "read", "update", "delete", "list"]
-}
-EOP
+// # Create and manage entities and groups
+// path "identity/*" {
+//    capabilities = ["create", "read", "update", "delete", "list"]
+// }
 
-}
+// # Manage tokens
+// path "auth/token/*" {
+//    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+// }
 
-resource "vault_token" "project1-namespace-admin-token" {
-  policies = ["default",vault_policy.project1-namespace-admin-policy.name]
-  renewable = true
-  ttl = "96h"
-  renew_min_lease = 43200
-  renew_increment = 86400
-}
+// # Manage secrets at 'kv'
+// path "kv/*" {
+//    capabilities = ["create", "read", "update", "delete", "list"]
+// }
+// EOP
 
-resource "tfe_variable" "project1-workspace-namespace-vault-token" {
-  workspace_id=tfe_workspace.project1-workspace.id
-  description="Admin Vault Token "
-  category="env"
-  key="VAULT_TOKEN"
-  value=vault_token.project1-namespace-admin-token.client_token
-}
+// }
+
+// resource "vault_token" "project1-namespace-admin-token" {
+//   policies = ["default",vault_policy.project1-namespace-admin-policy.name]
+//   renewable = true
+//   ttl = "96h"
+//   renew_min_lease = 43200
+//   renew_increment = 86400
+// }
+
+// resource "tfe_variable" "project1-workspace-namespace-vault-token" {
+//   workspace_id=tfe_workspace.project1-workspace.id
+//   description="Admin Vault Token "
+//   category="env"
+//   key="VAULT_TOKEN"
+//   value=vault_token.project1-namespace-admin-token.client_token
+// }
 
 // resouce "vault_identity_entity" "project1-namespace-admin-identity-entity" {
 //   name="project1-namespace-admin-identity-entity"
