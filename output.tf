ee26769469b0f401df6d9d3048c681e38710b761
Yulei Liu <yulei.liu@gmail.com>
2020-05-26 23:18:36 +1000

    add github url to output

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "vault_token" {
     value = vault_token.project1-namespace-admin-token.client_token
 }
+
+output "github_repo_url" {
+    vaule = "Your github url is: ${github_repository.cba-vault-namespace-nsproject1.http_clone_url}"
+}
