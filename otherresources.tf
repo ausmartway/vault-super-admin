c2dbe589b1185d34dc4059cfdc018847f750ecde
Yulei Liu <yulei.liu@gmail.com>
2020-05-26 22:59:19 +1000

    add output

--- otherresources.tf
+++ otherresources.tf
@@ -20,80 +20,74 @@ resource "tfe_workspace" "project1-workspace" {
   }
 }
 
-// resource "tfe_variable" "project1-workspace-namespace-vault-namespace" {
-//   workspace_id=tfe_workspace.project1-workspace.id
-//   description="namespace this workspace is bind to"
-//   category="env"
-//   key="VAULT_NAMESPACE"
-//   value=vault_namespace.project1-namespace.path
-// }
+resource "tfe_variable" "project1-workspace-namespace-vault-namespace" {
+  workspace_id=tfe_workspace.project1-workspace.id
+  description="namespace this workspace is bind to"
+  category="env"
+  key="VAULT_NAMESPACE"
+  value=vault_namespace.project1-namespace.path
+}
 
-// resource "vault_namespace" "project1-namespace" {
-//   path = "project1"
-// }
+# Vault
+resource "vault_namespace" "project1-namespace" {
+  path = "project1"
+}
 
 
-// resource "vault_policy" "project1-namespace-admin-policy" {
-//   name="project1-namespace-admin-policy"
-//   policy=<<EOP
-// # Manage namespaces
-// path "sys/namespaces/*" {
-//    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-// }
+resource "vault_policy" "project1-namespace-admin-policy" {
+  name="project1-namespace-admin-policy"
+  policy=<<EOP
+# Manage namespaces
+path "${vault_namespace.project1-namespace.path}/sys/namespaces/*" {
+   capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
 
-// # Manage policies
-// path "sys/policies/acl/*" {
-//    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-// }
+# Manage policies
+path "${vault_namespace.project1-namespace.path}/sys/policies/acl/*" {
+   capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
 
-// # List policies
-// path "sys/policies/acl" {
-//    capabilities = ["list"]
-// }
+# List policies
+path "${vault_namespace.project1-namespace.path}/sys/policies/acl" {
+   capabilities = ["list"]
+}
 
-// # Enable and manage secrets engines
-// path "sys/mounts/*" {
-//    capabilities = ["create", "read", "update", "delete", "list"]
-// }
+# Enable and manage secrets engines
+path "${vault_namespace.project1-namespace.path}/sys/mounts/*" {
+   capabilities = ["create", "read", "update", "delete", "list"]
+}
 
-// # List available secrets engines
-// path "sys/mounts" {
-//   capabilities = [ "read" ]
-// }
+# List available secrets engines
+path "${vault_namespace.project1-namespace.path}/sys/mounts" {
+  capabilities = [ "read" ]
+}
 
-// # Create and manage entities and groups
-// path "identity/*" {
-//    capabilities = ["create", "read", "update", "delete", "list"]
-// }
+# Create and manage entities and groups
+path "${vault_namespace.project1-namespace.path}/identity/*" {
+   capabilities = ["create", "read", "update", "delete", "list"]
+}
 
-// # Manage tokens
-// path "auth/token/*" {
-//    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-// }
+# Manage tokens
+path "${vault_namespace.project1-namespace.path}/auth/token/*" {
+   capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
 
-// # Manage secrets at 'kv'
-// path "kv/*" {
-//    capabilities = ["create", "read", "update", "delete", "list"]
-// }
-// EOP
+# Manage secrets at '*'
+path "${vault_namespace.project1-namespace.path}/*" {
+   capabilities = ["create", "read", "update", "delete", "list"]
+}
+EOP
 
-// }
+}
 
-// resource "vault_token" "project1-namespace-admin-token" {
-//   policies = ["default",vault_policy.project1-namespace-admin-policy.name]
-//   renewable = true
-//   ttl = "96h"
-//   renew_min_lease = 43200
-//   renew_increment = 86400
-// }
-
-// resource "tfe_variable" "project1-workspace-namespace-vault-token" {
-//   workspace_id=tfe_workspace.project1-workspace.id
-//   description="Admin Vault Token "
-//   category="env"
-//   key="VAULT_TOKEN"
-//   value=vault_token.project1-namespace-admin-token.client_token
-// }
+resource "vault_token" "project1-namespace-admin-token" {
+  policies = ["default",vault_policy.project1-namespace-admin-policy.name]
+  renewable = true
+  no_parent=true
+  ttl = "96h"
+  renew_min_lease = 43200
+  renew_increment = 86400
+}
 
 // resouce "vault_identity_entity" "project1-namespace-admin-identity-entity" {
 //   name="project1-namespace-admin-identity-entity"
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "vault_token" {
+    value = vault_token.project1-namespace-admin-token.client_token
+}
