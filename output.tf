6819b24dac1e191457c7a3b6a09f9d3abc8463d7
Yulei Liu <yulei.liu@gmail.com>
2020-05-26 23:20:18 +1000

    typo

--- output.tf
+++ output.tf
@@ -3,5 +3,5 @@ output "vault_token" {
 }
 
 output "github_repo_url" {
-    vaule = "Your github url is: ${github_repository.cba-vault-namespace-nsproject1.http_clone_url}"
+    value = "Your github url is: ${github_repository.cba-vault-namespace-nsproject1.http_clone_url}"
 }
