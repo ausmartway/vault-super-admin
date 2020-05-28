e188f4c13145786c90380d06a44d5da4e85e4118
Yulei Liu <yulei.liu@gmail.com>
2020-05-29 09:17:06 +1000

    add more variables

--- output.tf
+++ output.tf
@@ -5,3 +5,17 @@ output "vault_token" {
 output "github_repo_url" {
     value = "Your github url is: ${github_repository.cba-vault-namespace-nsproject1.http_clone_url}"
 }
+
+output "github_repo_full_name" {
+    value = "github_repository.cba-vault-namespace-nsproject1.full_name"
+}
+
+output "tfe_worksapce_id" {
+    value=tfe_workspace.project1-workspace.id
+}
+
+
+output "vault_namespace_path" {
+    value=vault_namespace.project1-namespace.path
+}
+
