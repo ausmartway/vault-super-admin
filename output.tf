b4ca877fff36a39579ffd4f9e952e687060df4ff
Yulei Liu <yulei.liu@gmail.com>
2020-05-29 09:19:17 +1000

    typo

--- output.tf
+++ output.tf
@@ -3,11 +3,11 @@ output "vault_token" {
 }
 
 output "github_repo_url" {
-    value = "Your github url is: ${github_repository.cba-vault-namespace-nsproject1.http_clone_url}"
+    value = github_repository.cba-vault-namespace-nsproject1.http_clone_url
 }
 
 output "github_repo_full_name" {
-    value = "github_repository.cba-vault-namespace-nsproject1.full_name"
+    value = github_repository.cba-vault-namespace-nsproject1.full_name
 }
 
 output "tfe_worksapce_id" {
