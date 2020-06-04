a51da7083177fe4551005d10640c77f3b7f93be4
Yulei Liu <yulei.liu@gmail.com>
2020-06-04 16:22:34 +1000

    namespaceforcba creation

--- main.tf
+++ main.tf
@@ -100,39 +100,39 @@ resource "tfe_variable" "project1-workspace-namespace-vault-token" {
   sensitive=true
 }
 
-// //--------------------------------------------------------------------
-// // Variables
+//--------------------------------------------------------------------
+// Variables
 
 
 
 
-// //--------------------------------------------------------------------
-// // Modules
-// module "github_repo_module" {
-//   source  = "app.terraform.io/customer-demo-yulei-management-org/github-repo-module/specialcustomer"
-//   version = "0.0.3"
+//--------------------------------------------------------------------
+// Modules
+module "github_repo_module" {
+  source  = "app.terraform.io/customer-demo-yulei-management-org/github-repo-module/specialcustomer"
+  version = "0.0.3"
 
-//   reponame = "repositoryfor-${module.vault_namespace_module.vault-namespace}"
-// }
+  reponame = "repositoryfor-${module.vault_namespace_module.vault-namespace}"
+}
 
-// module "tfe_vault_workspace_module" {
-//   source  = "app.terraform.io/customer-demo-yulei-management-org/tfe-vault-workspace-module/specialcustomer"
-//   version = "0.0.4"
+module "tfe_vault_workspace_module" {
+  source  = "app.terraform.io/customer-demo-yulei-management-org/tfe-vault-workspace-module/specialcustomer"
+  version = "0.0.4"
 
-//   github-repo-fullname = "${module.github_repo_module.repoidentifier}"
-//   oauth-token-id = "ot-Xsx6ReJAouFcMzYN"
-//   tfe-organization = "customer-demo-yulei-consumer-org"
-//   tfe-workspace-name = "workspacefor-${module.vault_namespace_module.vault-namespace}"
-//   vault-namespace = "${module.vault_namespace_module.vault-namespace}"
-//   vault-token = "${module.vault_namespace_module.vault-token}"
-// }
+  github-repo-fullname = "${module.github_repo_module.repoidentifier}"
+  oauth-token-id = "ot-Xsx6ReJAouFcMzYN"
+  tfe-organization = "customer-demo-yulei-consumer-org"
+  tfe-workspace-name = "workspacefor-${module.vault_namespace_module.vault-namespace}"
+  vault-namespace = "${module.vault_namespace_module.vault-namespace}"
+  vault-token = "${module.vault_namespace_module.vault-token}"
+}
 
-// module "vault_namespace_module" {
-//   source  = "app.terraform.io/customer-demo-yulei-management-org/vault-namespace-module/specialcustomer"
-//   version = "0.0.2"
+module "vault_namespace_module" {
+  source  = "app.terraform.io/customer-demo-yulei-management-org/vault-namespace-module/specialcustomer"
+  version = "0.0.2"
 
-//   namespace = "namespaceforcba"
-// }
+  namespace = "namespaceforcba"
+}
 
 // //--------------------------------------------------------------------
 // // Modules
