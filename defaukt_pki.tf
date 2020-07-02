8cfd9b65169cd5772041fafc63893ad19bd7c706
Yulei Liu <yulei.liu@gmail.com>
2020-07-02 16:17:55 +1000

    typo

--- defaukt_pki.tf
+++ defaukt_pki.tf
@@ -4,7 +4,7 @@ resource vault_mount "default_pki" {
 }
 
 resource vault_pki_secret_backend_root_cert "ca" {
-  backend = vault_mount.pki.path
+  backend = vault_mount.default_pki.path
 
   type = "internal"
   common_name = "http://scb-vault.yulei.aws.hashidemos.io/"
@@ -20,7 +20,7 @@ resource vault_pki_secret_backend_root_cert "ca" {
 
 
 resource vault_pki_secret_backend_config_urls "ca_config_urls" {
-  backend              = vault_mount.pki.path
+  backend              = vault_mount.default_pki.path
   issuing_certificates = [
     "${var.vault_url}/v1/pki/ca",
 
@@ -28,7 +28,7 @@ resource vault_pki_secret_backend_config_urls "ca_config_urls" {
 }
 
 resource vault_pki_secret_backend_role "nginx" {
-  backend = vault_mount.pki.path
+  backend = vault_mount.default_pki.path
   name    = "nginx"
   allow_subdomains = true
   allow_any_name = false
