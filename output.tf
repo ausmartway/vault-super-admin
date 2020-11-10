c26138841f3ee6e4ae287b226f4648137096a250
Yulei Liu <yulei.liu@gmail.com>
2020-11-10 18:28:13 +1100

    disable output

--- output.tf
+++ output.tf
@@ -1,21 +1,21 @@
-output "vault_token" {
-    value = vault_token.prototype1-namespace-admin-token.client_token
-}
+// output "vault_token" {
+//     value = vault_token.prototype1-namespace-admin-token.client_token
+// }
 
-output "github_repo_url" {
-    value = github_repository.scb-vault-namespace-nsprototype1.http_clone_url
-}
+// output "github_repo_url" {
+//     value = github_repository.scb-vault-namespace-nsprototype1.http_clone_url
+// }
 
-output "github_repo_full_name" {
-    value = github_repository.scb-vault-namespace-nsprototype1.full_name
-}
+// output "github_repo_full_name" {
+//     value = github_repository.scb-vault-namespace-nsprototype1.full_name
+// }
 
-output "tfe_worksapce_id" {
-    value=tfe_workspace.prototype1-workspace.id
-}
+// output "tfe_worksapce_id" {
+//     value=tfe_workspace.prototype1-workspace.id
+// }
 
 
-output "vault_namespace_path" {
-    value=vault_namespace.prototype1-namespace.path
-}
+// output "vault_namespace_path" {
+//     value=vault_namespace.prototype1-namespace.path
+// }
 
