c6c33cfa8c1e1a6e28ae00c31fe11742d0754680
Yulei Liu <yulei.liu@gmail.com>
2020-11-18 09:28:54 +1100

    add vault-addr variable

--- main.tf
+++ main.tf
@@ -139,6 +139,7 @@ module "vault_namespace_selfservice_module_CGM" {
   source  = "app.terraform.io/customer-demo-yulei-management-org/vault-namespace-selfservice-module/specialcustomer"
   version = "0.0.9"
   namespace-name = "CGM"
+  vault-addr="http://192.168.57.14:8200"
 }
 
 
